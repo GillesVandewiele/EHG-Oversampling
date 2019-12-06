587c384a7dedea8388ccd33c4b320a9552428203
Gilles Vandewiele <givdwiel.vandewiele@ugent.be>
2019-12-06 14:02:33 +0100

    fmed, fpeak, sampen, rms now give similar/correlated results + autocorrelation zero-crossing

--- features.py
+++ features.py
@@ -1,105 +1,61 @@
 import numpy as np
 from scipy.signal.windows import hann
+from entropy import app_entropy
 
 
-def rms(signal, window_size=300, shift=15, hz=20):
-	# TODO: The Hanning windowing is optional and should be configurable
-    window = hann(window_size)
-    rms_signal = []
-    for i in range(0, len(signal) - window_size + 1, shift):
-        subsignal = signal[i:i+window_size] * window
-        rms_signal.append(np.sqrt(np.mean(subsignal ** 2)))
-    rms_signal = np.array(rms_signal)
-    rms_signal = np.interp(list(range(len(signal))),
-                           list(range(0, len(signal) - window_size + 1, shift)),
-                           rms_signal)
-    return rms_signal
+def rms(signal):
+    return np.sqrt(np.mean(signal[2000:-2000] ** 2)) * 1000
 
 
-def rowwise_chebyshev(x, y):
-    return np.max(np.abs(x - y), axis=1)
-
-
-def delay_embedding(data, emb_dim, lag=1):
-    data = np.asarray(data)
-    min_len = (emb_dim - 1) * lag + 1
-    if len(data) < min_len:
-        msg = "cannot embed data of length {} with embedding dimension {} " \
-            + "and lag {}, minimum required length is {}"
-        raise ValueError(msg.format(len(data), emb_dim, lag, min_len))
-    m = len(data) - min_len + 1
-    indices = np.repeat([np.arange(emb_dim) * lag], m, axis=0)
-    indices += np.arange(m).reshape((m, 1))
-    return data[indices]
-
-
-def sampen(data, m=3, tol=0.15, dist=rowwise_chebyshev):
-    data = np.asarray(data)
-    
-    tol *= np.std(data)
-      
-    n = len(data)
-    tVecs = delay_embedding(np.asarray(data), m, lag=1)
-    counts = []
-    
-    # Calculate c_{m} and c_{m - 1}
-    for m in [m - 1, m]:
-        counts.append(0)
-        # get the matrix that we need for the current m
-        tVecsM = tVecs[:, :m]
-        # successively calculate distances between each pair of template vectors
-        for i in range(1, len(tVecsM)):
-            dsts = dist(np.roll(tVecsM, i, axis=0), tVecsM) 
-            # count how many distances are smaller than the tolerance
-            counts[-1] += np.sum(dsts <= tol)
-            
-    if counts[1] == 0 or counts[0] == 0:
-        # log would be infinite => cannot determine saen
-        saen = -np.log((n - m) / (n - m - 1))
-    else:
-        saen = -np.log(counts[1] / counts[0])
-    return saen
-
-def calc_sampen(window, m=3, r=0.15):
-    n_channels = len(window)
-    features = []
-    names = []
-    
-    for ch in range(n_channels):
-        names.append('sampen_{}'.format(ch))
-        features.append(sampen(window[ch], m=m, tol=r))
-        
-    return features, names
-
-
-def median_freq(data, low, high, fs):
+def median_freq(data, fs):
     ps = np.abs(np.fft.fft(data))**2
     M = len(ps)
     freqs = np.fft.fftfreq(M, d=1/fs)
     
-    start = np.argmax(freqs >= low)
-    end = np.argmin(freqs <= high)
+    mask = freqs >= 0
+    freqs = freqs[mask]
+    ps = ps[mask]
     
     best_k, min_dist = None, float('inf')
     
-    for k in range(start, end):
-        d = abs(np.sum(ps[start:k]) - np.sum(ps[k:end]))
+    # Divide-and-conquer on array of positive values to find
+    # index that partitions the array into parts with
+    # an as equal sum as possible
+    k = len(ps) // 2
+    offset = len(ps) // 4
+    while offset > 0:
+        sum1 = np.sum(ps[:k])
+        sum2 = np.sum(ps[k:])
+        d = abs(sum1 - sum2)
+            
         if d < min_dist:
             min_dist = d
             best_k = k
+        
+        if sum1 > sum2:
+            k -= offset
+        else:
+            k += offset
+            
+        offset = offset // 2
             
     return freqs[best_k]
 
 
-def peak_freq(data, low, high, fs):
+def peak_freq(data, fs):
     ps = np.abs(np.fft.fft(data))**2
     M = len(ps)
     freqs = np.fft.fftfreq(M, d=1/fs)
     
-    start = np.argmax(freqs >= low)
-    end = np.argmin(freqs <= high)
+    mask = freqs >= 0
+    freqs = freqs[mask]
+    ps = ps[mask]
     
-    return np.max(ps[start:end] / np.max(ps))
+    return np.abs(freqs[np.argmax(ps)])
+
+
+def sampen(data):
+    return app_entropy(signal_ch1[3000:-3000], 2, r=0.15)
 
 
 def log2(data):
